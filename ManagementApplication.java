0b7548a9ece83d6be3d907625971d0ce89407be3
sivasubramanian m <121441414+sivasubaramanian@users.noreply.github.com>
2024-02-01 16:02:30 +0530

    Update ManagementApplication.java

--- ManagementApplication.java
+++ ManagementApplication.java
@@ -1,17 +1,1 @@
-package com.asset.management;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
-// import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-// import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-
-@SpringBootApplication //(exclude = {DataSourceAutoConfiguration.class, JpaRepositoriesAutoConfiguration.class})
-// @EnableMongoRepositories(basePackages = "com.asset.management.repository")
-public class ManagementApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ManagementApplication.class, args);
-	}
-
-}
